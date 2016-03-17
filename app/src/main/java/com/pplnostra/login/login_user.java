414963282778d3ac4f66b917bb1a7f5d42354833
martin novela <martinovela507@gmail.com>
2016-03-17 08:25:52 +0700

    front end untuk login dan completing form

--- app/src/main/java/com/pplnostra/login/login_user.java
+++ app/src/main/java/com/pplnostra/login/login_user.java
@@ -7,31 +7,33 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class login_user extends AppCompatActivity implements View.OnClickListener{
 
-
-    TextView tvSignUpLink;
+    Button bFacebookSignIn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_user);
 
-        tvSignUpLink = (TextView) findViewById(R.id.tvSignUpLink);
+        bFacebookSignIn = (Button)findViewById(R.id.bFacebookSignIn);
 
-        tvSignUpLink.setOnClickListener(this);
-
+        bFacebookSignIn.setOnClickListener(this);
 
     }
 
+
+
+    @Override
     public void onClick(View v) {
         switch (v.getId()){
-
-            case R.id.tvSignUpLink:
+            case R.id.bFacebookSignIn:
                 startActivity(new Intent(this, com.pplnostra.login.CompletingForm.class));
                 break;
+
         }
     }
 }
